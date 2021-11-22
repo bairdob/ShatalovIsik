1d1384987a850a6f3368c0a06d2eac276e1a120a
bairdob <krumane@mail.ru>
2021-11-22 23:29:38 +0300

    add city pixel coordinates

--- lr1/src/main.cpp
+++ lr1/src/main.cpp
@@ -18,66 +18,73 @@ const string LL_LON = "CORNER_LL_LON_PRODUCT";
 const string LR_LAT = "CORNER_LR_LAT_PRODUCT";
 const string LR_LON = "CORNER_LR_LON_PRODUCT";
 
-const float city_latitude = 51.8333;
-const float city_lantitude = 107.6167;
+const float cityLatitude = 51.8333;
+const float cityLontitude = 107.6167;
 
 
 class Picture {
-private:
+public:
 	string txtFilePath;
+	string tifFilePath;
 	map<string, double> angles;
-	map<string, double> delta;
+	vector<Point2f>  corners; // LL, UR, UL, LR
+	map<string, double> deltaAngles;
+	map<string, double> deltaCorners;
 
 public:
-	Picture(string str){
-		txtFilePath = str;
+	Picture(string txtFile, string tifFile){
+		txtFilePath = txtFile;
+		tifFilePath = tifFile;
 	};
 
 	void setData(){
-		setAngles();
-		setDelta();
+		readAngles();
+		setDeltaAngles();
+		setCorners();
+		setDeltaCorners();
 	}
 
-	void setAngles(){
+	void readAngles(){
 		std::ifstream infile(txtFilePath);
+
 		if (!infile){
-			std::cout << "Error in opening file !" << std::endl;            
-	    	}
+			std::cout << "Error: in opening *.txt file" << std::endl;            
+	    }
 
 		string line; 
 		while (std::getline(infile, line)){
-			if (getCornerFromLine(line, UL_LAT) != -1){
-				angles[UL_LAT] = getCornerFromLine(line, UL_LAT);
+			if (getAngleFromLine(line, UL_LAT) != -1){
+				angles[UL_LAT] = getAngleFromLine(line, UL_LAT);
 			}
-			if (getCornerFromLine(line, UL_LON) != -1){
-				angles[UL_LON] = getCornerFromLine(line, UL_LON);
-			}
-
-
-			if (getCornerFromLine(line, UR_LAT) != -1){
-				angles[UR_LAT] = getCornerFromLine(line, UR_LAT);
-			}
-			if (getCornerFromLine(line, UR_LON) != -1){
-				angles[UR_LON] = getCornerFromLine(line, UR_LON);
+			if (getAngleFromLine(line, UL_LON) != -1){
+				angles[UL_LON] = getAngleFromLine(line, UL_LON);
 			}
 
-			if (getCornerFromLine(line, LL_LAT) != -1){
-				angles[LL_LAT] = getCornerFromLine(line, LL_LAT);
+
+			if (getAngleFromLine(line, UR_LAT) != -1){
+				angles[UR_LAT] = getAngleFromLine(line, UR_LAT);
 			}
-			if (getCornerFromLine(line, LL_LON) != -1){
-				angles[LL_LON] = getCornerFromLine(line, LL_LON);
+			if (getAngleFromLine(line, UR_LON) != -1){
+				angles[UR_LON] = getAngleFromLine(line, UR_LON);
+			}
+
+			if (getAngleFromLine(line, LL_LAT) != -1){
+				angles[LL_LAT] = getAngleFromLine(line, LL_LAT);
+			}
+			if (getAngleFromLine(line, LL_LON) != -1){
+				angles[LL_LON] = getAngleFromLine(line, LL_LON);
 			}			
 
-			if (getCornerFromLine(line, LR_LAT) != -1){
-				angles[LR_LAT] = getCornerFromLine(line, LR_LAT);
+			if (getAngleFromLine(line, LR_LAT) != -1){
+				angles[LR_LAT] = getAngleFromLine(line, LR_LAT);
 			}
-			if (getCornerFromLine(line, LR_LON) != -1){
-				angles[LR_LON] = getCornerFromLine(line, LR_LON);
+			if (getAngleFromLine(line, LR_LON) != -1){
+				angles[LR_LON] = getAngleFromLine(line, LR_LON);
 			}				
 	    }   
 	};
 
-	double getCornerFromLine(string line, string target){
+	double getAngleFromLine(string line, string target){
 	    size_t pos = line.find(target);
 	    double num = -1;
 
@@ -88,75 +95,87 @@ public:
 	    return num;
 	}	
 
-	void setDelta(){
-		delta["DELTA_LAT"] = abs(angles[UR_LAT] - angles[LL_LAT]);
-		delta["DELTA_LON"] = abs(angles[UR_LON] - angles[LL_LON]);
-	}
-
-	void printAngles(){
+	void printMap(map<string, double> temp){
 		cout.precision(10);
-		for (const auto& [key, value] : angles){
+		for (const auto& [key, value] : temp){
 			std::cout << key << " = " << value << endl;
 		}
 		cout << "\n";
 	}
 
+	void setDeltaAngles(){
+		deltaAngles["DELTA_ANGLE_LAT"] = abs(angles[UL_LAT] - angles[UR_LAT]);
+		deltaAngles["DELTA_ANGLE_LON"] = abs(angles[UL_LON] - angles[LL_LON]);
+	}
 
-	void printDelta(){
-		cout.precision(10);
-		for (const auto& [key, value] : delta){
-			std::cout << key << " = " << value << endl;
-		}
-		cout << "\n";		
+	void setCorners(){
+		Mat imgOriginal = imread(tifFilePath, IMREAD_COLOR);
+
+		if(imgOriginal.empty()) {
+			cout << "Error: the image has been incorrectly loaded." << endl;
+		} 
+	    
+		Mat thresh;
+		cvtColor(imgOriginal, thresh, COLOR_BGR2GRAY);
+		threshold(thresh, thresh, 0, 255, THRESH_BINARY);
+		medianBlur(thresh, thresh, 5);
+		goodFeaturesToTrack(thresh, corners, 10, 0.5, 3000, Mat(), 20, 3, false, 0.1);
+	}
+
+	void setDeltaCorners(){
+		deltaCorners["DELTA_CORNER_X"] = abs(corners[1].x - corners[2].x);
+		deltaCorners["DELTA_CORNER_Y"] = abs(corners[2].y - corners[0].y);
+	}
+
+	void printCorners(){
+		cout << "** Number of corners detected: " << corners.size() << endl;
+		cout << corners << endl;
+	}
+
+	void cityCoordinates(){
+		cout << "City pixel coordinates:" << endl;
+		double k1 = deltaCorners["DELTA_CORNER_X"] / deltaAngles["DELTA_ANGLE_LAT"] ;
+		double k2 = deltaCorners["DELTA_CORNER_Y"] / deltaAngles["DELTA_ANGLE_LON"];
+
+		cout << abs(angles[UL_LAT] - cityLatitude) * deltaCorners["DELTA_CORNER_X"] / 2 << endl;
+		cout << abs(angles[UL_LON] - cityLontitude) * deltaCorners["DELTA_CORNER_Y"] / 2 << endl;
 	}
 };
 
 
 int main(){
-	Picture p("../data/LE07_L2SP_131024_20021005_20200916_02_T1_MTL.txt");
+	Picture p("../data/LE07_L2SP_131024_20021005_20200916_02_T1_MTL.txt", 
+			"../data/LE07_L2SP_131024_20021005_20200916_02_T1_SR_B1.TIF");
 	p.setData();
-	p.printAngles();
-	p.printDelta();
+	p.printMap(p.angles);
+	p.printMap(p.deltaAngles);
+	p.printCorners();
+	p.printMap(p.deltaCorners);
+	p.cityCoordinates();
 
 	Mat imgOriginal = imread("../data/LE07_L2SP_131024_20021005_20200916_02_T1_SR_B1.TIF", IMREAD_COLOR);
+	Mat imgCopy = imgOriginal.clone();
 
-	if(imgOriginal.empty()) {
-		cout << "Error: the image has been incorrectly loaded." << endl;
-		return 0; 
-	} 
-    
-	Mat thresh;
-	cvtColor(imgOriginal, thresh, COLOR_BGR2GRAY);
-	threshold(thresh, thresh, 0, 255, THRESH_BINARY);
-	medianBlur(thresh, thresh, 5);
-
-	vector<Point2f> corners;
-	Mat copy = imgOriginal.clone();
-
-	goodFeaturesToTrack(thresh, corners, 10, 0.5, 3000, Mat(), 20, 3, false, 0.1 );
-  
-	cout << "** Number of corners detected: " << corners.size() << endl;
-	for( size_t i = 0; i < corners.size(); i++ ){
-		circle(copy, corners[i], 50, Scalar(0, 255, 0), FILLED );
+	for(size_t i = 0; i < p.corners.size(); i++){
+		circle(imgCopy, p.corners[i], 50, Scalar(0, 255, 0), FILLED );
 	}
-	cout << corners << endl;
+
+	Point2f ptCity(2000 , 3180); 
+	circle(imgCopy, ptCity, 50, Scalar(0, 0, 255), FILLED);
+
+	Point2f ptCityCalculated(2472 , 2924); 
+	circle(imgCopy, ptCityCalculated, 50, Scalar(255, 0, 255), FILLED);
+    
+	Rect rect(1750, 2950, 500, 500);
+	rectangle(imgCopy, rect, Scalar(0, 255, 255), 5,LINE_8);
 
 	Mat crop = imgOriginal(Range(1700,2000),Range(3000,3100));
+    crop = imgOriginal(rect).clone();
 
-	// cv::Point2f pt(2000 , 3180);
-	// circle(copy, pt, 50, Scalar(0, 0, 255), FILLED );
-
-	cout << corners[2].x << " " << corners[2].y << endl;
-	cv::Point2f pt(6191 , 7549);
-	circle(copy, pt, 50, Scalar(0, 0, 255), FILLED );
-    
-	cv::Rect rect(1750, 2950, 500, 500);
-	rectangle(copy, rect, cv::Scalar(0, 255, 255), 5,LINE_8);
-
-    // copy = imgOriginal(rect).clone();
-	cv::imshow("Output", copy);
-	imwrite("test.jpg", copy);
-	cv::waitKey(0);
+	imshow("Output", imgCopy);
+	imshow("crop", crop);
+	imwrite("original.jpg", imgCopy);
+	waitKey(0);
 
 	return 0;
 }
